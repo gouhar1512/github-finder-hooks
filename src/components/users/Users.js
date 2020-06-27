2c162f414f5d54a0ba6eb66c34d74673cd0bd8f6
Gouhar <ushran987@gmail.com>
2020-06-28 02:51:16 +0530

    all refactored to hooks

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import UserItem from "./UserItem";
 import Spinner from "../layout/Spinner";
-import PropTypes from "prop-types";
+import GithubContext from "../../context/github/githubContext";
 
-function Users(props) {
-  const { users, loading } = props;
+function Users() {
+  const githubContext = useContext(GithubContext);
+
+  const { users, loading } = githubContext;
 
   if (loading) {
     return <Spinner />;
@@ -13,16 +15,11 @@ function Users(props) {
   return (
     <div style={userStyle}>
       {users.map((user) => (
-        <UserItem key={user.id} user={user}/>
+        <UserItem key={user.id} user={user} />
       ))}
     </div>
   );
 }
-
-Users.propType = {
-  users: PropTypes.array.isRequired,
-  loading: PropTypes.bool.isRequired,
-};
 
 const userStyle = {
   //style object
